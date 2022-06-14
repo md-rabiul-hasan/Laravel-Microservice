9c0a98f703ad15245d2e34c0b438d72b1ffe7a43
md-rabiul-hasan <mdrabiulhasan.me@gmail.com>
2022-06-14 12:09:34 +0600

    migration factory seeder

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -9,7 +9,7 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory;
 
     /**
      * The attributes that are mass assignable.
@@ -37,4 +37,6 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public $timestamp = false;
 }
